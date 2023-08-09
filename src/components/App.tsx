705349a43c1f29c89e1d02b2decf004b352ef858
Humberto Piaia <betopiaia@gmail.com>
2023-08-09 18:31:16 -0300

    update

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,17 +1,15 @@
-import { OrbitControls } from "@react-three/drei";
-import { Canvas } from "@react-three/fiber";
-import { Perf } from "r3f-perf";
-import { NoToneMapping, Vector3 } from "three";
+import { OrbitControls } from '@react-three/drei'
+import { Canvas } from '@react-three/fiber'
+import { NoToneMapping, Vector3 } from 'three'
 
-import { PortalScene } from "./PortalScene";
+import { PortalScene } from './PortalScene'
 
 export default function App() {
   return (
-    <Canvas flat id="canvas" gl={{ toneMapping: NoToneMapping }} camera={{ position: new Vector3(3, 3, 3) }}>
-      <color attach="background" args={["#171720"]} />
-      <OrbitControls />
+    <Canvas id="canvas" gl={{ toneMapping: NoToneMapping }} camera={{ position: new Vector3(3, 3, 3) }}>
+      <color attach="background" args={['#171720']} />
+      <OrbitControls enablePan={false} maxPolarAngle={Math.PI / 2 - 0.1} />
       <PortalScene />
-      <Perf position="top-left" />
     </Canvas>
-  );
+  )
 }
--- src/components/PortalScene.tsx
+++ src/components/PortalScene.tsx
@@ -1,46 +1,44 @@
-import { Sparkles, useGLTF, useTexture } from "@react-three/drei";
-import { type GLTF } from "three-stdlib";
+import { Sparkles, useGLTF, useTexture } from '@react-three/drei'
+import { type GLTF } from 'three-stdlib'
 
 type GLTFResult = GLTF & {
   nodes: {
-    baked: THREE.Mesh;
-    poleLightA: THREE.Mesh;
-    poleLightB: THREE.Mesh;
-    portalLight: THREE.Mesh;
-  };
-};
-
-export function PortalScene() {
-  const texture = useTexture("/model/baked.jpg");
-  const model = useGLTF("/model/portal.glb") as GLTFResult;
-
-  return (
-    <>
-      <group>
-        <mesh geometry={model.nodes.baked.geometry}>
-          <meshBasicMaterial map={texture} map-flipY={false} />
-        </mesh>
-
-        <mesh geometry={model.nodes.poleLightA.geometry} position={model.nodes.poleLightA.position}>
-          <meshBasicMaterial color="#ffffe5" />
-        </mesh>
-
-        <mesh geometry={model.nodes.poleLightB.geometry} position={model.nodes.poleLightB.position}>
-          <meshBasicMaterial color="#ffffe5" />
-        </mesh>
-
-        <mesh
-          geometry={model.nodes.portalLight.geometry}
-          position={model.nodes.portalLight.position}
-          rotation={model.nodes.portalLight.rotation}
-        >
-          <meshBasicMaterial color="white" />
-        </mesh>
-
-        <Sparkles size={6} scale={[4, 2, 4]} position-y={1} speed={0.2} count={40} />
-      </group>
-    </>
-  );
+    baked: THREE.Mesh
+    poleLightA: THREE.Mesh
+    poleLightB: THREE.Mesh
+    portalLight: THREE.Mesh
+  }
 }
 
-useGLTF.preload("/model/portal.glb");
+export function PortalScene() {
+  const texture = useTexture('/model/baked.jpg')
+  const model = useGLTF('/model/portal.glb') as GLTFResult
+
+  return (
+    <group>
+      <mesh geometry={model.nodes.baked.geometry}>
+        <meshBasicMaterial map={texture} map-flipY={false} />
+      </mesh>
+
+      <mesh geometry={model.nodes.poleLightA.geometry} position={model.nodes.poleLightA.position}>
+        <meshBasicMaterial color="#ffffe5" />
+      </mesh>
+
+      <mesh geometry={model.nodes.poleLightB.geometry} position={model.nodes.poleLightB.position}>
+        <meshBasicMaterial color="#ffffe5" />
+      </mesh>
+
+      <mesh
+        geometry={model.nodes.portalLight.geometry}
+        position={model.nodes.portalLight.position}
+        rotation={model.nodes.portalLight.rotation}
+      >
+        <meshBasicMaterial color="#ffffff" />
+      </mesh>
+
+      <Sparkles size={6} scale={[4, 2, 4]} position-y={1} speed={0.2} count={40} />
+    </group>
+  )
+}
+
+useGLTF.preload('/model/portal.glb')
