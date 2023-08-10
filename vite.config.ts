49eab7f3f615b314d6b11f8dfd5dd95bd1c8f015
Humberto Piaia <betopiaia@gmail.com>
2023-08-10 20:13:51 -0300

    format code

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 import glsl from 'vite-plugin-glsl'
 
 export default defineConfig({
